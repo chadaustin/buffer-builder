ffb2d7a223213472db07db5908643160d7de6044
Chad Austin <chad@chadaustin.me>
2015-01-30 00:24:16 -0800

    ByteStringBuilder is slightly faster: 6.98 us per iteration -> 2.48 us

--- Data/BufferWriter.hs
+++ Data/BufferWriter.hs
@@ -8,19 +8,21 @@ module Data.BufferWriter
     , appendBS
     ) where
 
-import Data.Monoid ((<>))
+import Data.Monoid ((<>), mempty)
 import Data.Char (ord)
 import Data.Word (Word8)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Builder as BSB
 import qualified Control.Monad.State.Lazy as State
 
-type BufferWriter = State.State BS.ByteString ()
+type BufferWriter = State.State BSB.Builder ()
 
 runBufferWriter :: BufferWriter -> BS.ByteString
-runBufferWriter st = snd $ State.runState st ""
+runBufferWriter st = BSL.toStrict $ BSB.toLazyByteString $ snd $ State.runState st mempty
 
 appendByte :: Word8 -> BufferWriter
-appendByte b = State.modify' (\s -> BS.snoc s b)
+appendByte b = State.modify' (<> BSB.word8 b)
 
 -- | Unsafe conversion between 'Char' and 'Word8'. This is a no-op and
 -- silently truncates to 8 bits Chars > '\255'. It is provided as
@@ -33,4 +35,4 @@ appendChar8 :: Char -> BufferWriter
 appendChar8 = appendByte . c2w
 
 appendBS :: BS.ByteString -> BufferWriter
-appendBS bs = State.modify' (<> bs)
+appendBS bs = State.modify' (<> BSB.byteString bs)
