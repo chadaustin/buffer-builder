68ecfddf2e4955da63abd89b16817f7ef5a1bacb
Chad Austin <chad@chadaustin.me>
2015-02-05 13:35:19 -0800

    begin splitting tests into multiple files

---
+++ test/BufferTest.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE MagicHash, OverloadedStrings, TemplateHaskell #-}
+
+module BufferTest (tests, main) where
+
+import Test.Tasty
+import Test.Tasty.TH
+import Test.Tasty.HUnit
+import Data.BufferBuilder
+
+case_append_bytes :: Assertion
+case_append_bytes = do
+    let result = runBufferBuilder $ do
+            appendChar8 'f'
+            appendChar8 'o'
+            appendChar8 'o'
+    assertEqual "matches" "foo" result
+
+case_append_string :: Assertion
+case_append_string = do
+    let result = runBufferBuilder $ do
+            appendChar8 'f'
+            appendChar8 'o'
+            appendChar8 'o'
+            appendBS "bar"
+    assertEqual "matches" "foobar" result
+
+case_append_literals :: Assertion
+case_append_literals = do
+    let result = runBufferBuilder $ do
+            appendLiteral "foo"#
+            appendLiteral "bar"#
+    assertEqual "matches" "foobar" result
+
+tests :: TestTree
+tests = $(testGroupGenerator)
+
+main :: IO ()
+main = defaultMain tests
--- test/Main.hs
+++ test/Main.hs
@@ -1,36 +1,11 @@
 {-# LANGUAGE TemplateHaskell, OverloadedStrings, MagicHash #-}
 
 import Test.Tasty
-import Test.Tasty.TH
-import Test.Tasty.HUnit
-import Data.BufferBuilder
 
-case_append_bytes :: Assertion
-case_append_bytes = do
-    let result = runBufferBuilder $ do
-            appendChar8 'f'
-            appendChar8 'o'
-            appendChar8 'o'
-    assertEqual "matches" "foo" result
+import qualified BufferTest as BufferTest
 
-case_append_string :: Assertion
-case_append_string = do
-    let result = runBufferBuilder $ do
-            appendChar8 'f'
-            appendChar8 'o'
-            appendChar8 'o'
-            appendBS "bar"
-    assertEqual "matches" "foobar" result
-
-case_append_literals :: Assertion
-case_append_literals = do
-    let result = runBufferBuilder $ do
-            appendLiteral "foo"#
-            appendLiteral "bar"#
-    assertEqual "matches" "foobar" result
-
-tests :: TestTree
-tests = $(testGroupGenerator)
+allTests :: TestTree
+allTests = testGroup "all tests" [ BufferTest.tests ]
 
 main :: IO ()
-main = defaultMain tests
+main = defaultMain allTests
