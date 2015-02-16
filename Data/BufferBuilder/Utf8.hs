74801f39d1e65176999366c263203aa31c35717d
Chad Austin <chad@chadaustin.me>
2015-02-15 23:31:11 -0800

    bump version, document UTF-8 builder

--- Data/BufferBuilder/Utf8.hs
+++ Data/BufferBuilder/Utf8.hs
@@ -69,15 +69,19 @@ runUtf8Builder a = BB.runBufferBuilder $ unBuilder a
 
 -- Text encoding
 
--- TODO: optimize with custom encoder
+-- TODO: optimize appendText with custom UTF-16 -> UTF-8 encoder in C
+
+-- | Encodes the given 'Text' in UTF-8, appending it to the buffer.
 appendText :: Text -> Utf8Builder ()
 appendText a = Utf8Builder $ BB.appendBS $ encodeUtf8 a
 {-# INLINE appendText #-}
 
+-- | Encodes the given 'String' in UTF-8, appending it to the buffer.
 appendString :: String -> Utf8Builder ()
 appendString s = mapM_ appendChar s
 {-# INLINABLE appendString #-}
 
+-- | Encodes a single 'Char' in UTF-8, appending it to the buffer.
 appendChar :: Char -> Utf8Builder ()
 appendChar c = Utf8Builder $ BB.appendCharUtf8 c
 {-# INLINE appendChar #-}
@@ -85,20 +89,24 @@ appendChar c = Utf8Builder $ BB.appendCharUtf8 c
 
 -- ASCII-7
 
--- | Appends the bottom 7 bits of byte to the buffer.
+-- | Appends the bottom 7 bits of a byte to the buffer.
 appendByte7 :: Word8 -> Utf8Builder ()
 appendByte7 = Utf8Builder . BB.appendByte7
 {-# INLINE appendByte7 #-}
 
--- | Appends the bottom 7 bits of the 'Char' to the buffer.
+-- | Appends the bottom 7 bits of a 'Char' to the buffer.
 appendChar7 :: Char -> Utf8Builder ()
 appendChar7 = Utf8Builder . BB.appendChar7
 {-# INLINE appendChar7 #-}
 
+-- | Appends the given ByteString to the buffer, taking the bottom
+-- 7 bits of each byte.
 appendBS7 :: ByteString -> Utf8Builder ()
 appendBS7 = Utf8Builder . BB.appendBS7
 {-# INLINE appendBS7 #-}
 
+-- | Appends the zero-terminated byte string at the given address
+-- to the buffer, taking the bottom 7 bits of each byte.
 appendLiteral7 :: Addr# -> Utf8Builder ()
 appendLiteral7 addr = Utf8Builder $ BB.appendLiteral7 addr
 {-# INLINE appendLiteral7 #-}
@@ -131,22 +139,36 @@ appendEscapedJsonText txt = Utf8Builder $ BB.appendEscapedJsonText txt
 
 -- Unsafe
 
+-- | Directly append a byte into the UTF-8 code stream.  Incorrect use of
+-- this function can result in invalid UTF-8.
 unsafeAppendByte :: Word8 -> Utf8Builder ()
 unsafeAppendByte = Utf8Builder . BB.appendByte
 {-# INLINE unsafeAppendByte #-}
 
+-- | Directly append the bottom 8 bits of the given character to the UTF-8
+-- code stream.  Incorrect use of this function can result in invalid UTF-8.
 unsafeAppendChar8 :: Char -> Utf8Builder ()
 unsafeAppendChar8 = Utf8Builder . BB.appendChar8
 {-# INLINE unsafeAppendChar8 #-}
 
+-- | Directly append the zero-terminated byte sequence pointed to by
+-- the given address.  Be careful that the referenced byte sequence
+-- contains valid UTF-8.
 unsafeAppendLiteral :: Addr# -> Utf8Builder ()
 unsafeAppendLiteral addr = Utf8Builder $ BB.appendLiteral addr
 {-# INLINE unsafeAppendLiteral #-}
 
+-- | Directly append the given byte sequence pointed to by the given address.
+-- Be careful that the referenced byte sequence contains valid UTF-8.
+--
+-- __WARNING__: passing an incorrect length value is likely to cause an access
+-- violation or worse.
 unsafeAppendLiteralN :: Int -> Addr# -> Utf8Builder ()
 unsafeAppendLiteralN !len addr = Utf8Builder $ BB.unsafeAppendLiteralN len addr
 {-# INLINE unsafeAppendLiteralN #-}
 
+-- | Directly append the given 'ByteString' to the output buffer.
+-- Be careful that the referenced 'ByteString' contains valid UTF-8.
 unsafeAppendBS :: ByteString -> Utf8Builder ()
 unsafeAppendBS a = Utf8Builder $ BB.appendBS a
 {-# INLINE unsafeAppendBS #-}
